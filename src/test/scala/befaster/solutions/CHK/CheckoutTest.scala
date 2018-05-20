eec5af95ed3ee32e46c46b81752a095d1154cdd2
sbx_user1059 <sbx_user1059@ip-10-11-129-188.eu-west-2.compute.internal>
2018-05-20 17:23:33 +0000

    Sun May 20 17:23:33 UTC 2018

--- src/test/scala/befaster/solutions/CHK/CheckoutTest.scala
+++ src/test/scala/befaster/solutions/CHK/CheckoutTest.scala
@@ -3,5 +3,21 @@ package befaster.solutions.CHK
 import org.scalatest.{FlatSpec, Matchers}
 
 class CheckoutTest extends FlatSpec with Matchers {
-  it should "" {}
+  it should "calculate total price without discount" in {
+    val skus = "A,B,C,D"
+    val expectedTotalPrice = 115
+
+    val result = Checkout.checkout(skus)
+
+    result shouldBe expectedTotalPrice
+  }
+
+  it should "calculate total price with discount" in {
+    val skus = "A,B,C,A,D,B,A"
+    val expectedTotalPrice = 210
+
+    val result = Checkout.checkout(skus)
+
+    result shouldBe expectedTotalPrice
+  }
 }

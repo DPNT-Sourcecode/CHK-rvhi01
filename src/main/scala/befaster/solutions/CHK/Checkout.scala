f9bac9e65b8dab422aed067962a13b101d23ec7b
sbx_user1059 <sbx_user1059@ip-10-11-129-188.eu-west-2.compute.internal>
2018-05-20 17:44:33 +0000

    Sun May 20 17:44:33 UTC 2018

--- src/main/scala/befaster/solutions/CHK/Checkout.scala
+++ src/main/scala/befaster/solutions/CHK/Checkout.scala
@@ -1,7 +1,40 @@
 package befaster.solutions.CHK
 
-import befaster.runner.SolutionNotImplementedException
+case class SKU(item: String, price: Int)
+
+case class Discount(item: String, quantity: Int, price: Int)
 
 object Checkout {
-  def checkout(skus: String): Integer = throw new SolutionNotImplementedException()
+  private val prices = List(
+    SKU("A", 50),
+    SKU("B", 30),
+    SKU("C", 20),
+    SKU("D", 15)
+  )
+
+  private val discounts = List(
+    Discount("A", 3, 130),
+    Discount("B", 2, 45)
+  )
+
+  def checkout(skus: String): Integer = {
+    val items = skus.split(",").toList
+
+    val groupedSkus = items.flatMap(i => prices.filter(p => p.item == i)).groupBy(_.item)
+
+    val temp = groupedSkus.map { sku =>
+      val discount = discounts.find(d => d.item == sku._1) match {
+        case Some(d) =>
+          val discountCount = sku._2.length / d.quantity
+          val remainderCount = sku._2.length % d.quantity
+
+          discountCount * d.price + remainderCount * sku._2.head.price
+        case None => sku._2.map(_.price).sum
+      }
+
+      discount
+    }
+
+    temp.sum
+  }
 }
--- src/test/scala/befaster/solutions/CHK/CheckoutTest.scala
+++ src/test/scala/befaster/solutions/CHK/CheckoutTest.scala
@@ -5,19 +5,25 @@ import org.scalatest.{FlatSpec, Matchers}
 class CheckoutTest extends FlatSpec with Matchers {
   it should "calculate total price without discount" in {
     val skus = "A,B,C,D"
-    val expectedTotalPrice = 115
 
     val result = Checkout.checkout(skus)
 
-    result shouldBe expectedTotalPrice
+    result shouldBe 115
   }
 
   it should "calculate total price with discount" in {
     val skus = "A,B,C,A,D,B,A"
-    val expectedTotalPrice = 210
 
     val result = Checkout.checkout(skus)
 
-    result shouldBe expectedTotalPrice
+    result shouldBe 210
+  }
+
+  it should "return -1 in case of illegal input" in {
+    val skus = "A,B,C,X"
+
+    val result = Checkout.checkout(skus)
+
+    result shouldBe -1
   }
 }
